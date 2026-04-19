09813d71386ee0cd67af5feb6cf578d45074a996
8t-truck <kwoncar3@gmail.com>
2026-04-19 21:46:57 +0900

    set MDI_basic

--- Project3_MDI/Project3_MDI/Form1.cs
+++ Project3_MDI/Project3_MDI/Form1.cs
@@ -6,5 +6,10 @@ namespace Project3_MDI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Project3_MDI/Project3_MDI/Form2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3_MDI
+{
+    public partial class Child : Form
+    {
+        Child child;
+        public Child()
+        {
+            InitializeComponent();
+        }
+    }
+}
